fab10ea8e69490fac501395011d257352f79fad4
Unknown <oded.antar97@gmail.com>
2018-10-10 19:15:54 +0300

    Stage the build onto the server

--- project/server/index.js
+++ project/server/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.use(express.static(__dirname +'./../client/public'));
+app.use(express.static(__dirname +'/build'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
